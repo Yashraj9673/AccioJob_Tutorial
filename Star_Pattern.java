843125ad38b0899e078c613e6ecb05195cdb83d6
Yashraj9673 <yashrajchaudhari48924@gmail.com>
2022-10-11 21:21:15 +0530

    initial commit

---
+++ Star_Pattern.java
@@ -0,0 +1,7 @@
+public class Star_Pattern {
+    public static void main(String[] args) throws java.lang.Exception {
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println("*****");
+    }
+}
