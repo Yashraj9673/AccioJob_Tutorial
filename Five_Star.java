bbfffd2c8b3405b8b6bd41c0fd2d8205e623df2c
Yashraj9673 <yashrajchaudhari48924@gmail.com>
2022-10-11 16:24:31 +0530

    initial commit

---
+++ Five_Star.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("* * * * *");
+    System.out.print("*\n*\n*\n*\n*\n");
+	}
+}
